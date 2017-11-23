1dcb304ff11747f4c248cda555ea37cd888cf61a
aleksandramaria <ola.ksi@gmail.com>
2017-11-23 20:55:26 +0000

    new commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://guarded-basin-33278.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://guarded-basin-33278.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
